7753ff20b80471194eec466dd731ef9dc47c0c81
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-08-08 03:33:53 +0200

    structs, interfaces & methods

---
+++ learnGoWithTests/Structs/shapes.go
@@ -0,0 +1,28 @@
+package structs
+
+import "math"
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Height * r.Width
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Height + r.Width)
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
