b46b56d223c91459bf8010a307693be53616e805
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-08-13 01:52:00 +0200

    initial code for sync

---
+++ learnGoWithTests/sync/sync_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"sync"
+	"testing"
+)
+
+func TestCounter(t *testing.T) {
+	t.Run("it runs safely concurrently", func(t *testing.T) {
+		wantedCount := 1000
+		counter := Counter{}
+
+		var wg sync.WaitGroup
+		wg.Add(wantedCount)
+
+		for i := 0; i < wantedCount; i++ {
+			go func() {
+				counter.Inc()
+				wg.Done()
+			}()
+		}
+		wg.Wait()
+
+		assertCounter(t, counter, wantedCount)
+	})
+
+}
+
+func assertCounter(t testing.TB, got Counter, want int) {
+	t.Helper()
+	if got.Value() != want {
+		t.Errorf("got %d, want %d", got.Value(), want)
+	}
+}
