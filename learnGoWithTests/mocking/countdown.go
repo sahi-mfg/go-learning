e0ba979a697f23369e9d3eefe7b7f50ccd83193a
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-08-12 16:01:16 +0200

    enhance tests with mocking

--- learnGoWithTests/mocking/countdown.go
+++ learnGoWithTests/mocking/countdown.go
@@ -44,6 +44,23 @@ const sleep = "sleep"
 const finalWord = "Go!"
 const countdownStart = 3
 
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
